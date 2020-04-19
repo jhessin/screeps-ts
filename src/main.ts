fcd1e47347c39eeb47ae14367189bf3233f89058
Jim Hessin <jhessin@gmail.com>
2020-04-19 00:24:38 -0600

    Added terminal code

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,51 @@ export const loop = ErrorMapper.wrapLoop(() => {
     spawn.run();
   }
 
-  // Run Links
   for (const room of Object.values(Game.rooms)) {
+    // Run Links
     for (const link of room.find(FIND_MY_STRUCTURES, {
       filter: s => s instanceof StructureLink,
     }) as StructureLink[]) {
       link.run();
+    }
+
+    // Run Terminals
+    for (const terminal of room.find(FIND_MY_STRUCTURES, {
+      filter: s => s instanceof StructureTerminal,
+    }) as StructureTerminal[]) {
+      if (terminal.cooldown > 0) break;
+      let roomName = terminal.room.name;
+      for (const resource of Object.keys(
+        terminal.store,
+      ) as ResourceConstant[]) {
+        if (resource === RESOURCE_ENERGY) continue;
+        let amount = terminal.store[resource];
+        let orders = Game.market.getAllOrders({
+          type: ORDER_BUY,
+          resourceType: resource,
+        });
+
+        let best: Order | undefined;
+        for (const order of orders) {
+          if (!best) best = order;
+          if (!order.roomName) continue;
+          if (!best.roomName) continue;
+          let orderCost = Game.market.calcTransactionCost(
+            amount,
+            roomName,
+            order.roomName,
+          );
+          let bestCost = Game.market.calcTransactionCost(
+            amount,
+            roomName,
+            best.roomName,
+          );
+          if (order.price > best.price || orderCost < bestCost) best = order;
+        }
+        if (!best || !best.roomName) continue;
+
+        Game.market.deal(best.id, amount, roomName);
+      }
     }
   }
 
